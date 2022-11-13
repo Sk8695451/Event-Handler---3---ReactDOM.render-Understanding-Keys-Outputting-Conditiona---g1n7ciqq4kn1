46f405861aacb3b6bdb1216c48b0e978ffb8bd94
unknown <sk8695451>
2022-11-13 22:03:11 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,32 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const handleInput = (event) =>{
-   // use console.log
+  const handleInput = (event) => {
+    // if (event.target.value == 'k')  
+    if (event.target.id = "text-input") {
+      // event.target.value = 'ok';
+      console.log(event.target.value);
+      return;
+    }
+    // if (event.target.value == 5) {
+    else if (event.target.id = "num-input") {
+      // event.target.value = 15;
+      console.log(event.target.value);
+    }
   }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <input id="text-input" type={'text'} onChange={handleInput} />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <input id="num-input" type={'number'} onChange={handleInput} />
+      <br />
     </div>
   )
 }
